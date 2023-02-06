cdf88e5434aafb847faf6fae5494b886f9ab5567
R_Diney <rafael_diney@hotmail.com>
2023-02-06 17:50:41 -0300

    Interligando as classes de dominio

--- desafio-dio-poo/src/Main.java
+++ desafio-dio-poo/src/Main.java
@@ -1,7 +1,9 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -26,9 +28,41 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        // System.out.println(curso1);
+        // System.out.println(curso2);
+        // System.out.println(mentoria);
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devRafael = new Dev();
+        devRafael.setNome("Rafael");
+        devRafael.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devRafael.getConteudosInscritos());
+
+        devRafael.progredir();
+        devRafael.progredir();
+
+        System.out.println("-");
+        System.out.println("Conteúdos Concluídos" + devRafael.getConteudosConcluidos());
+        System.out.println("Conteúdos Inscritos" + devRafael.getConteudosInscritos());
+        System.out.println("XP: " + devRafael.calcularTotalXp());
+
+        System.out.println("---------------------------");
+
+        Dev devDiney = new Dev();
+        devDiney.setNome("Diney");
+        devDiney.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devDiney.getConteudosInscritos());
+        devDiney.progredir();
+        devDiney.progredir();
+        devDiney.progredir();
+        System.out.println("Conteúdos Concluídos" + devDiney.getConteudosConcluidos());
+        System.out.println("Conteúdos Inscritos" + devDiney.getConteudosInscritos());
+        System.out.println("XP: " + devDiney.calcularTotalXp());
     }
 }
